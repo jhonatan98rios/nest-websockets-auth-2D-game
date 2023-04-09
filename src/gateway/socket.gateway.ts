3144a5f773b575b6dd45515dad27dcff8264fd3d
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-04-09 14:24:01 -0300

    add data on ConnectedPlayers

--- src/gateway/socket.gateway.ts
+++ src/gateway/socket.gateway.ts
@@ -1,6 +1,14 @@
-import { OnModuleInit } from '@nestjs/common'
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, ConnectedSocket, OnGatewayConnection } from "@nestjs/websockets";
 import { Server, Socket } from 'socket.io'
+
+
+type Player = {
+    id: string
+}
+
+type ConnectedPlayers = {
+    [key: string]: Player
+  }
 
 @WebSocketGateway({})
 export class SocketGateway implements OnGatewayConnection {
@@ -8,13 +16,20 @@ export class SocketGateway implements OnGatewayConnection {
     @WebSocketServer()
     server: Server
 
+    connectedPlayers: ConnectedPlayers
+
+    constructor() {
+        this.connectedPlayers = {}
+    }
 
     handleConnection(client: any, ...args: any[]) {
-        console.log(client.id)
 
+        this.connectedPlayers[client.id] = ({ id: client.id })
+        
         this.server.emit('actor::connected', {
             actor: client.id,
-        })
+            connectedPlayers: Object.values(this.connectedPlayers)
+        })        
     }
 
     handleDisconnect(client: Socket){
@@ -23,15 +38,15 @@ export class SocketGateway implements OnGatewayConnection {
         this.server.emit('actor::disconnected', {
             actor: client.id,
         })
-    }
 
+        delete this.connectedPlayers[client.id]
+    }
 
     @SubscribeMessage('player::moved')
     onMovePlayer(
         @MessageBody() body: any, 
         @ConnectedSocket() client: Socket
     ) {
-
         const { payload } = body
 
         this.server.emit('actor::moved', {
@@ -39,5 +54,4 @@ export class SocketGateway implements OnGatewayConnection {
             actor: client.id,
         })
     }
-
 }
